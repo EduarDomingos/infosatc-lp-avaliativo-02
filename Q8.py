df7149d4cfa0c5d859367cb4eb3d5aa1ee5f4840
EduarDomingos <eduardo.246715@alunosatc.edu.br>
2021-09-09 16:36:55 -0300

    Update Q8
    Update Q8

--- Q8.py
+++ Q8.py
@@ -1,2 +1,7 @@
 Pc = ["gabinete cooler master", 'Placa-Mãe ASRock', "Processador AMD Ryzen ", "placa de vídeo GeForce RTX 2080Ti","Memória XPG Gammix D20", "Fonte XPG Core Reactor 850W", "SSD WD Black AN1500 2TB"]
+
+print(Pc)
+del Pc[3]
+print(Pc)
+Pc.insert(1, "placa de video GTX 1050")  # 
 print(Pc)
